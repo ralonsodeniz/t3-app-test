f1b2f236a0928dff564ba103f2c4f8b1af9bdf0a
ralonsodeniz <ralonsodeniz@gmail.com>
2024-05-05 19:46:26 +0200

    chore(db): deduping

--- src/server/db/queries/image.ts
+++ src/server/db/queries/image.ts
@@ -1,11 +1,26 @@
+import { cache } from "react";
 import { unstable_cache as nextCache } from "next/cache";
+
 import { db } from "@/server/db";
 
-export const getImages = nextCache(
-  async () =>
-    db.query.images.findMany({
-      orderBy: (image, { desc }) => desc(image.id),
-    }),
-  ["images"],
-  { tags: ["images"], revalidate: 30 },
+export const getImages = cache(
+  nextCache(
+    async () =>
+      db.query.images.findMany({
+        orderBy: (image, { desc }) => desc(image.id),
+      }),
+    ["images"],
+    { tags: ["images"], revalidate: 30 },
+  ),
+);
+
+export const getImage = cache((id: number) =>
+  nextCache(
+    async () =>
+      db.query.images.findFirst({
+        where: (image, { eq }) => eq(image.id, id),
+      }),
+    ["image", `${id}`],
+    { tags: ["image", `${id}`], revalidate: 30 },
+  )(),
 );
